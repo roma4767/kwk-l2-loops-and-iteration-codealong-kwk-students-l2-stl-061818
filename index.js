54a010993150aea66667cfa9c8810cfa9a146e1a
Romina D-R <rdr2711@yahoo.com>
2018-06-22 20:32:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+function tailsNeverFails() {
+  let tails = 0;
+  
+  while (Math.random() >= 0.5) {
+    tails++
+  }
+  
+  console.log(`You got ${tails} tails in a row`);
+}
+
+
+tailsNeverFails();
